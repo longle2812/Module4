58b776e5047e7d03832daec8f54cd968db906bed
Long Le <lehailong2812@gmail.com>
2021-06-22 14:20:55 +0700

    [Thực hành] Validating Form Input

---
+++ form-validation-srping/src/main/java/com/codegym/model/User.java
@@ -0,0 +1,79 @@
+package com.codegym.model;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.*;
+
+
+public class User {
+    @NotEmpty
+    @Size(min = 5, max = 45)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 5, max = 45)
+    private String lastName;
+
+    @NotNull
+    private String phoneNumber;
+
+    @Min(18)
+    private int age;
+
+    @NotEmpty
+    @Pattern(regexp = "@.*?\\.")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String phoneNumber, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
